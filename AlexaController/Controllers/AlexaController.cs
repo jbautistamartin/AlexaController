c48d983106f09631f95efa7f96377991d3b7c8f4
José Luis Bautista Martín <jbautista@bansi.com.mx>
2024-11-14 16:31:06 -0600

    Se agrega el Controlador de Alexa

---
+++ AlexaController/Controllers/AlexaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlexaController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlexaController : ControllerBase
+    {
+
+        //ApagarEquipo
+
+        //ReiniciarEquipo
+
+        //IniciarSteam
+
+        //CerrarSteam
+
+        //ReiniciarSteam
+
+        //IniciarModoJuegos
+
+        //DetenerModoJuegos
+
+        //CerrarRetroArch
+
+
+    }
+}
